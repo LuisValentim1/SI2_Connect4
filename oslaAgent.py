ac72c4933203cb43905e6ee8888aa1237a661ea1
LuisValentim1 <60874150+LuisValentim1@users.noreply.github.com>
2022-05-17 15:16:24 +0100

    osla Agent implementation

---
+++ oslaAgent.py
@@ -0,0 +1,68 @@
+import random
+from move import Move
+import pickle
+import copy 
+
+
+class RLAgent:
+
+    def __init__(self, player_num):
+        self.name = "osla Agent"
+        self.no_seq3 = 0
+        self.player_num = player_num
+        
+
+    def play(self, board, move_dict):
+        
+        return self.getBestMove(board)
+
+    def getBestMove(self, board):
+        moveValues = []
+        for move in range(0,7):
+            value = self.evalBoard(board, move, self.player_num)
+            moveValues.append(value)
+
+        bestMoveVal = 0
+        bestMove = 0
+        for val in moveValues:
+            if val > bestMoveVal:
+                bestMoveVal = val 
+                bestMove = moveValues.index(val)
+
+        return bestMove
+
+    def evalBoard(self, board, move, player):
+        simBoard = copy.deepcopy(board)
+        simBoard.placePiece(move)
+        simBoard.checkWinner()
+        if simBoard.winner == player:
+            return 10000
+
+        if self.blockedSeq3(board, (3%player)+1, move):
+            return 1000
+
+        if self.madeSeq3(board, simBoard, player):
+            return 100
+
+        if self.madeSeq(board, simBoard, player):
+            return 10 
+
+        return 1
+
+    def blockedSeq3(self, board, opPlayer, move):
+        simBoard = copy.deepcopy(board)
+        simBoard.placePiece(move)
+        simBoard.checkWinner()
+        if(simBoard.winner == opPlayer):
+            return True
+
+    def madeSeq3(self, board, simulatedBoard, player):
+        if(len(board.getSequencesFromPlayerWithLength(self, 3, player)) < len(simulatedBoard.getSequencesFromPlayerWithLength(self, 3, player))):
+            return True
+
+    def madeSeq(self, board, simulatedBoard, player):
+        if(len(simulatedBoard.getSequencesFromPlayer(self, player))-len(board.getSequencesFromPlayer(self, player)) == 2 ):
+            return True
+
+    def print_board(self,board):
+        [print(b.fill,end=",") for b in board]
